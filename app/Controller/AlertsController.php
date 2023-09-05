54dc724420b3442c81e84b2412f359df3ab410a6
弄月 <{ID}+{username}@users.noreply.github.com>
2023-09-05 22:13:18 +0800

    add validate

--- app/Controller/AlertsController.php
+++ app/Controller/AlertsController.php
@@ -5,16 +5,36 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\AlertsService;
+use App\Foundation\Traits\ValidateTrait;
 use Hyperf\Di\Annotation\Inject;
 
 class AlertsController extends AbstractController
 {
+    use ValidateTrait;
+
+    protected $validates = [
+        'index' => [
+            'status' => 'required',
+            'alerts' => 'required',
+            'externalURL' => 'required',
+        ],
+    ];
+
+    protected $messages = [
+        'status.required' => 'status required',
+        'alerts.required' => 'alerts required',
+        'externalURL.required' => 'externalURL required',
+    ];
+
     #[Inject]
     protected AlertsService $service;
 
     public function index()
     {
-        $this->service->handle($this->request->post());
+        $request = $this->request->post();
+        $this->validateData($request, 'index');
+
+        $this->service->handle($request);
         return [];
     }
 }
